cf3ece5b9b9f8e4db6ae289b04b03a051ea28a6c
shenkrad <58443488+Shenkrad@users.noreply.github.com>
2025-10-09 13:42:47 +0200

    feat: complete exercise 09_sumAll
    add js code to complete the task and pass the unit tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
 
+    if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber) 
+        || firstNumber < 0 || secondNumber < 0)
+    {
+        return 'ERROR';
+    }
+
+    if (firstNumber > secondNumber)
+    {
+        const middleValue = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = middleValue;
+    }
+
+    let sum = 0;
+    for(let i = firstNumber; i <= secondNumber; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
