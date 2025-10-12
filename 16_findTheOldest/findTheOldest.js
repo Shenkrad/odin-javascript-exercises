d91211eaa9b24739e19c5893cfb750bc2d7ec547
shenkrad <58443488+Shenkrad@users.noreply.github.com>
2025-10-12 21:42:38 +0200

    complete exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    const currentYear = new Date().getFullYear();
+
+    return people.reduce((oldest, person) => {
+        return ((person.yearOfDeath || currentYear) - person.yearOfBirth) > ((oldest.yearOfDeath || currentYear) - oldest.yearOfBirth) ? person : oldest;
+    });
 
 };
 
