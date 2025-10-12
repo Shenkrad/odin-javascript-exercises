780d7458b5f710a7e207468a1acaa6b8dc117505
shenkrad <58443488+Shenkrad@users.noreply.github.com>
2025-10-12 21:23:24 +0200

    feat: complete exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let indexNumber;
+    if (typeof countArg !== 'number') {
+        indexNumber = parseInt(index)
+    } else {
+        indexNumber = index
+    }
+    
+    if (indexNumber < 0) return "OOPS";
+    if (indexNumber == 0) return 0;
 
+    let num = 0;
+    let lastValue = 1;
+    let lastValue2 = 0;
+
+    for(let i = 1; i < indexNumber; i++) {
+        num = lastValue + lastValue2;
+        lastValue2 = lastValue;
+        lastValue = num;
+    }
+    return num;
 };
 
 // Do not edit below this line
