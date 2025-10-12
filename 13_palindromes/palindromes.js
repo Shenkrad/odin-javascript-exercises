a4a9967440d60dd984395fbfa988f1742c9b7c99
shenkrad <58443488+Shenkrad@users.noreply.github.com>
2025-10-12 21:04:37 +0200

    feat: complete exersice 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (givenString) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const clean = givenString
+        .toLowerCase()
+        .split("")
+        .filter((char) => alphanumerical.includes(char))
+        .join("");
+
+    const reverse = clean.split("").reverse().join("");
+
+    return clean === reverse;
 };
 
 // Do not edit below this line
